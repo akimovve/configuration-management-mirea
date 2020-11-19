f4965ff6c4c4cfad3b6fa656656b3618349d8251
akimovve <valery.e.akimov@gmail.com>
2020-11-19 13:43:04 +0300

    Add practice 9_10

---
+++ practice9_10/main.py
@@ -0,0 +1,49 @@
+import dis
+
+def ex_1(x):
+	return x * 10 + 42
+
+dis.dis(ex_1)
+
+def ex_2(n):
+	r = 1
+	while n > 1:
+		r *= n
+		n -= 1
+	return r
+
+print("\n\n")
+dis.dis(ex_2)
+
+"""
+ 11           0 LOAD_FAST                0 (x)
+              2 LOAD_CONST               1 (10)
+              4 BINARY_MULTIPLY
+              6 LOAD_CONST               2 (42)
+              8 BINARY_ADD
+             10 RETURN_VALUE
+
+
+
+  5           0 LOAD_CONST               1 (1)
+              2 STORE_FAST               1 (r)
+
+  6     >>    4 LOAD_FAST                0 (n)
+              6 LOAD_CONST               1 (1)
+              8 COMPARE_OP               4 (>)
+             10 POP_JUMP_IF_FALSE       30
+
+  7          12 LOAD_FAST                1 (r)
+             14 LOAD_FAST                0 (n)
+             16 INPLACE_MULTIPLY
+             18 STORE_FAST               1 (r)
+
+  8          20 LOAD_FAST                0 (n)
+             22 LOAD_CONST               1 (1)
+             24 INPLACE_SUBTRACT
+             26 STORE_FAST               0 (n)
+             28 JUMP_ABSOLUTE            4
+
+  9     >>   30 LOAD_FAST                1 (r)
+             32 RETURN_VALUE
+"""
